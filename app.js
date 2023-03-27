5a4bab924f4df6577f0fcc23159a9ac3eef0aa49
Enes <enes431811@gmail.com>
2023-03-27 17:23:24 +0300

    Project has been completed.

--- app.js
+++ app.js
@@ -4,47 +4,61 @@ const todoInput = document.getElementById("todo-input");
 
 const todoUl = document.getElementById("todo-ul");
 
+let todos = JSON.parse(localStorage.getItem("TODOS")) || [];
+
 addBtn.addEventListener("click", () => {
     if (todoInput.value.trim() === "") {
         alert("Please enter your new plan");
     } else {
         const newTodo = {
             id: new Date().getTime(),
+
             completed: false,
+
             text: todoInput.value,
         };
+
         createListElement(newTodo);
-        todoInput.value="";
+
+        todos.push(newTodo);
+
+        localStorage.setItem("TODOS", JSON.stringify(todos))
+
+        todoInput.value = "";
     }
 })
 
 const createListElement = (newTodo) => {
 
-    const {id,completed,text}=newTodo;
+    const {
+        id,
+        completed,
+        text
+    } = newTodo;
 
     const li = document.createElement("li");
 
     li.setAttribute("id", id);
 
-    completed ? li.classList.add("completed"):"";
-    
-    const okIcon=document.createElement("i");
+    completed ? li.classList.add("completed") : "";
 
-    okIcon.setAttribute("class","fas fa-check");
+    const okIcon = document.createElement("i");
+
+    okIcon.setAttribute("class", "fas fa-check");
 
     li.appendChild(okIcon);
 
-    const p =document.createElement("p");
+    const p = document.createElement("p");
 
     const pTextNode = document.createTextNode(text);
 
     p.appendChild(pTextNode);
 
-    li. appendChild(p);
+    li.appendChild(p);
 
-    const deleteIcon=document.createElement("i");
+    const deleteIcon = document.createElement("i");
 
-    deleteIcon.setAttribute("class","fas fa-trash");
+    deleteIcon.setAttribute("class", "fas fa-trash");
 
     li.appendChild(deleteIcon);
 
@@ -52,19 +66,33 @@ const createListElement = (newTodo) => {
 
 };
 
-todoUl.addEventListener("click", (event) =>{
+todoUl.addEventListener("click", (event) => {
+    const Id = event.target.parentElement.getAttribute("id");
+
     if (event.target.classList.contains("fa-trash")) {
         event.target.parentElement.remove();
+        todos = todos.filter((todo) => todo.id !== Number(Id));
+        localStorage.setItem("TODOS", JSON.stringify(todos));
     }
-    if(event.target.classList.contains("fa-check")){
+    if (event.target.classList.contains("fa-check")) {
         event.target.parentElement.classList.toggle("completed");
     }
-})
+});
 
-todoInput.addEventListener("keydown",(event)=>{
-    if (event.code==="Enter") {
+todoInput.addEventListener("keydown", (event) => {
+    if (event.code === "Enter") {
         addBtn.click();
     }
 });
 
-window.onload=()=>{todoInput.focus()};
+const renderSavedTodos = () => {
+    todos.forEach((todo) => {
+        createListElement(todo);
+    })
+}
+
+renderSavedTodos();
+
+window.onload = () => {
+    todoInput.focus()
+};
